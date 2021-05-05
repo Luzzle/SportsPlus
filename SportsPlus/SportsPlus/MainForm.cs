637ba7801c8c425765c8b9069cc78f44f6754f96
Luzzle <49113582+Luzzle@users.noreply.github.com>
2021-05-05 13:45:22 +1000

    Initial commit

---
+++ SportsPlus/SportsPlus/MainForm.cs
@@ -0,0 +1,27 @@
+﻿//
+//  SportsPlus - MainForm.cs
+//  Created by Cristian Lustri
+//  Copyright 2021 - All Rights Reserved
+//
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SportsPlus
+{
+    // MainForm Class - Inherits from class "Form"
+    public partial class MainForm : Form
+    {
+        // Form Class Constructor
+        public MainForm() {     
+            InitializeComponent();   
+        }
+
+        // Form Load Event
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
