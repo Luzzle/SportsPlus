73fc614e68639b55c26bba8aad0385ce37b12752
Luzzle <49113582+Luzzle@users.noreply.github.com>
2021-05-26 11:55:01 +1000

    Logevent progress

--- SportsPlus/SportsPlus/LogEvent.Designer.cs
+++ SportsPlus/SportsPlus/LogEvent.Designer.cs
@@ -33,7 +33,18 @@ namespace SportsPlus
             this.btnChangeEvent = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.lblAge = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -46,54 +57,155 @@ namespace SportsPlus
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1228, 104);
+            this.panel1.Size = new System.Drawing.Size(585, 104);
             this.panel1.TabIndex = 0;
             // 
             // btnChangeEvent
             // 
             this.btnChangeEvent.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.btnChangeEvent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnChangeEvent.Location = new System.Drawing.Point(1082, 37);
+            this.btnChangeEvent.Location = new System.Drawing.Point(438, 37);
             this.btnChangeEvent.Name = "btnChangeEvent";
             this.btnChangeEvent.Size = new System.Drawing.Size(134, 40);
             this.btnChangeEvent.TabIndex = 1;
             this.btnChangeEvent.Text = "Change Event";
             this.btnChangeEvent.UseVisualStyleBackColor = false;
+            this.btnChangeEvent.Click += new System.EventHandler(this.btnChangeEvent_Click);
             // 
             // lblTitle
             // 
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblTitle.Location = new System.Drawing.Point(24, 30);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(458, 48);
+            this.lblTitle.Size = new System.Drawing.Size(392, 48);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Log Event - ";
             // 
             // panel2
             // 
+            this.panel2.AutoScroll = true;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.lblAge);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.textBox6);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 104);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1228, 631);
-            this.panel2.TabIndex = 0;
+            this.panel2.Size = new System.Drawing.Size(585, 392);
+            this.panel2.TabIndex = 1;
             // 
-            // lblAge
+            // label6
             // 
-            this.lblAge.AutoSize = true;
-            this.lblAge.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblAge.Location = new System.Drawing.Point(24, 7);
-            this.lblAge.Name = "lblAge";
-            this.lblAge.Size = new System.Drawing.Size(92, 21);
-            this.lblAge.TabIndex = 0;
-            this.lblAge.Text = "Age Group: ";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(511, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Points";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(460, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Place";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(317, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "[ Time | Distance ]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "House";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Student Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Student ID";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(511, 36);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(45, 23);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(460, 36);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(45, 23);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(317, 36);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(137, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(226, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(85, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(125, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 23);
+            this.textBox1.TabIndex = 0;
             // 
             // LogEvent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1228, 735);
+            this.ClientSize = new System.Drawing.Size(585, 496);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "LogEvent";
@@ -112,6 +224,17 @@ namespace SportsPlus
         private System.Windows.Forms.Button btnChangeEvent;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
--- SportsPlus/SportsPlus/LogEvent.cs
+++ SportsPlus/SportsPlus/LogEvent.cs
@@ -8,8 +8,12 @@ using System.Windows.Forms;
 
 namespace SportsPlus
 {
+   
     public partial class LogEvent : Form
     {
+
+        int btnOffset = 0;
+
         Event loadedEvent;
         string loadedAge;
         public LogEvent(Event @event, string Age)
@@ -22,7 +26,17 @@ namespace SportsPlus
         private void LogEvent_Load(object sender, EventArgs e)
         {
             lblTitle.Text += loadedEvent.Event_Name;
-            lblAge.Text += loadedAge;
+            //lblAge.Text += loadedAge;
+        }
+
+        private void btnChangeEvent_Click(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.Location = new Point(24, 59 + btnOffset);
+            btn.Size = new Size(60, 23);
+            panel2.Controls.Add(btn);
+
+            btnOffset += btn.Size.Height + 1;
         }
     }
 }
