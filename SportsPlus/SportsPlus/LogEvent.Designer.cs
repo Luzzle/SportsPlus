3a4170958365efa66a6951c9b4341abab5e960fd
Luzzle <cristianlustri@gmail.com>
2021-06-01 06:29:58 +1000

    Event Log Progress 2

--- SportsPlus/SportsPlus/LogEvent.Designer.cs
+++ SportsPlus/SportsPlus/LogEvent.Designer.cs
@@ -30,30 +30,53 @@ namespace SportsPlus
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAddAthlete = new System.Windows.Forms.Button();
             this.btnChangeEvent = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.lblAge = new System.Windows.Forms.Label();
+            this.lblStuID = new System.Windows.Forms.Label();
+            this.stuID1 = new System.Windows.Forms.TextBox();
+            this.stuName1 = new System.Windows.Forms.TextBox();
+            this.lblStuName = new System.Windows.Forms.Label();
+            this.stuHouse1 = new System.Windows.Forms.TextBox();
+            this.lblStuHouse = new System.Windows.Forms.Label();
+            this.stuTD1 = new System.Windows.Forms.TextBox();
+            this.lblTD = new System.Windows.Forms.Label();
+            this.stuPlace1 = new System.Windows.Forms.TextBox();
+            this.lblPlace = new System.Windows.Forms.Label();
+            this.stuPoints1 = new System.Windows.Forms.TextBox();
+            this.lblPoints = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnAddAthlete);
             this.panel1.Controls.Add(this.btnChangeEvent);
             this.panel1.Controls.Add(this.lblTitle);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1228, 104);
+            this.panel1.Size = new System.Drawing.Size(642, 104);
             this.panel1.TabIndex = 0;
+            // 
+            // btnAddAthlete
+            // 
+            this.btnAddAthlete.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAddAthlete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddAthlete.Location = new System.Drawing.Point(495, 38);
+            this.btnAddAthlete.Name = "btnAddAthlete";
+            this.btnAddAthlete.Size = new System.Drawing.Size(134, 40);
+            this.btnAddAthlete.TabIndex = 2;
+            this.btnAddAthlete.Text = "Add Athlete";
+            this.btnAddAthlete.UseVisualStyleBackColor = false;
+            this.btnAddAthlete.Click += new System.EventHandler(this.btnAddAthlete_Click);
             // 
             // btnChangeEvent
             // 
             this.btnChangeEvent.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.btnChangeEvent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnChangeEvent.Location = new System.Drawing.Point(1082, 37);
+            this.btnChangeEvent.Location = new System.Drawing.Point(355, 38);
             this.btnChangeEvent.Name = "btnChangeEvent";
             this.btnChangeEvent.Size = new System.Drawing.Size(134, 40);
             this.btnChangeEvent.TabIndex = 1;
@@ -69,40 +92,130 @@ namespace SportsPlus
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Log Event - ";
             // 
-            // panel2
+            // lblStuID
             // 
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.lblAge);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 104);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1228, 631);
-            this.panel2.TabIndex = 0;
+            this.lblStuID.AutoSize = true;
+            this.lblStuID.Location = new System.Drawing.Point(12, 116);
+            this.lblStuID.Name = "lblStuID";
+            this.lblStuID.Size = new System.Drawing.Size(62, 15);
+            this.lblStuID.TabIndex = 1;
+            this.lblStuID.Text = "Student ID";
             // 
-            // lblAge
+            // stuID1
             // 
-            this.lblAge.AutoSize = true;
-            this.lblAge.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblAge.Location = new System.Drawing.Point(24, 7);
-            this.lblAge.Name = "lblAge";
-            this.lblAge.Size = new System.Drawing.Size(92, 21);
-            this.lblAge.TabIndex = 0;
-            this.lblAge.Text = "Age Group: ";
+            this.stuID1.Location = new System.Drawing.Point(12, 134);
+            this.stuID1.Name = "stuID1";
+            this.stuID1.Size = new System.Drawing.Size(100, 23);
+            this.stuID1.TabIndex = 2;
+            this.stuID1.TextChanged += new System.EventHandler(this.stuCheckID);
+            // 
+            // stuName1
+            // 
+            this.stuName1.Location = new System.Drawing.Point(118, 134);
+            this.stuName1.Name = "stuName1";
+            this.stuName1.ReadOnly = true;
+            this.stuName1.Size = new System.Drawing.Size(129, 23);
+            this.stuName1.TabIndex = 3;
+            // 
+            // lblStuName
+            // 
+            this.lblStuName.AutoSize = true;
+            this.lblStuName.Location = new System.Drawing.Point(118, 116);
+            this.lblStuName.Name = "lblStuName";
+            this.lblStuName.Size = new System.Drawing.Size(83, 15);
+            this.lblStuName.TabIndex = 4;
+            this.lblStuName.Text = "Student Name";
+            // 
+            // stuHouse1
+            // 
+            this.stuHouse1.Location = new System.Drawing.Point(253, 134);
+            this.stuHouse1.Name = "stuHouse1";
+            this.stuHouse1.ReadOnly = true;
+            this.stuHouse1.Size = new System.Drawing.Size(85, 23);
+            this.stuHouse1.TabIndex = 5;
+            // 
+            // lblStuHouse
+            // 
+            this.lblStuHouse.AutoSize = true;
+            this.lblStuHouse.Location = new System.Drawing.Point(253, 116);
+            this.lblStuHouse.Name = "lblStuHouse";
+            this.lblStuHouse.Size = new System.Drawing.Size(85, 15);
+            this.lblStuHouse.TabIndex = 6;
+            this.lblStuHouse.Text = "Student House";
+            // 
+            // stuTD1
+            // 
+            this.stuTD1.Location = new System.Drawing.Point(344, 134);
+            this.stuTD1.Name = "stuTD1";
+            this.stuTD1.Size = new System.Drawing.Size(139, 23);
+            this.stuTD1.TabIndex = 7;
+            // 
+            // lblTD
+            // 
+            this.lblTD.AutoSize = true;
+            this.lblTD.Location = new System.Drawing.Point(344, 116);
+            this.lblTD.Name = "lblTD";
+            this.lblTD.Size = new System.Drawing.Size(128, 15);
+            this.lblTD.TabIndex = 8;
+            this.lblTD.Text = "[Time / Distance Label]";
+            // 
+            // stuPlace1
+            // 
+            this.stuPlace1.Location = new System.Drawing.Point(489, 134);
+            this.stuPlace1.Name = "stuPlace1";
+            this.stuPlace1.Size = new System.Drawing.Size(53, 23);
+            this.stuPlace1.TabIndex = 9;
+            // 
+            // lblPlace
+            // 
+            this.lblPlace.AutoSize = true;
+            this.lblPlace.Location = new System.Drawing.Point(489, 116);
+            this.lblPlace.Name = "lblPlace";
+            this.lblPlace.Size = new System.Drawing.Size(35, 15);
+            this.lblPlace.TabIndex = 10;
+            this.lblPlace.Text = "Place";
+            // 
+            // stuPoints1
+            // 
+            this.stuPoints1.Location = new System.Drawing.Point(548, 134);
+            this.stuPoints1.Name = "stuPoints1";
+            this.stuPoints1.Size = new System.Drawing.Size(53, 23);
+            this.stuPoints1.TabIndex = 11;
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(548, 116);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(40, 15);
+            this.lblPoints.TabIndex = 12;
+            this.lblPoints.Text = "Points";
             // 
             // LogEvent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1228, 735);
-            this.Controls.Add(this.panel2);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(642, 659);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.stuPoints1);
+            this.Controls.Add(this.lblPlace);
+            this.Controls.Add(this.stuPlace1);
+            this.Controls.Add(this.lblTD);
+            this.Controls.Add(this.stuTD1);
+            this.Controls.Add(this.lblStuHouse);
+            this.Controls.Add(this.stuHouse1);
+            this.Controls.Add(this.lblStuName);
+            this.Controls.Add(this.stuName1);
+            this.Controls.Add(this.stuID1);
+            this.Controls.Add(this.lblStuID);
             this.Controls.Add(this.panel1);
             this.Name = "LogEvent";
             this.Text = "LogEvent";
             this.Load += new System.EventHandler(this.LogEvent_Load);
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,7 +224,18 @@ namespace SportsPlus
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnChangeEvent;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Button btnAddAthlete;
+        private System.Windows.Forms.Label lblStuID;
+        private System.Windows.Forms.TextBox stuID1;
+        private System.Windows.Forms.TextBox stuName1;
+        private System.Windows.Forms.Label lblStuName;
+        private System.Windows.Forms.TextBox stuHouse1;
+        private System.Windows.Forms.Label lblStuHouse;
+        private System.Windows.Forms.TextBox stuTD1;
+        private System.Windows.Forms.Label lblTD;
+        private System.Windows.Forms.TextBox stuPlace1;
+        private System.Windows.Forms.Label lblPlace;
+        private System.Windows.Forms.TextBox stuPoints1;
+        private System.Windows.Forms.Label lblPoints;
     }
 }
--- SportsPlus/SportsPlus/LogEvent.cs
+++ SportsPlus/SportsPlus/LogEvent.cs
@@ -12,6 +12,10 @@ namespace SportsPlus
     {
         Event loadedEvent;
         string loadedAge;
+
+        private int EntryOffset = 25;
+        private int AthleteCount = 1;
+
         public LogEvent(Event @event, string Age)
         {
             loadedEvent = @event;
@@ -21,8 +25,83 @@ namespace SportsPlus
 
         private void LogEvent_Load(object sender, EventArgs e)
         {
-            lblTitle.Text += loadedEvent.Event_Name;
-            lblAge.Text += loadedAge;
+            lblTitle.Text += loadedEvent.Event_Name + ": " + (loadedAge != "Opens" ? loadedAge + " yrs" : loadedAge);
+            lblTD.Text = (loadedEvent.Distance ? "Distance" : "Time");
+        }
+
+        private void AddAthlete()
+        {
+            AthleteCount++;
+
+            TextBox stuID = new TextBox();
+            stuID.Name = "stuID" + AthleteCount;
+            stuID.Size = new Size(100, 23);
+            stuID.Location = new Point(12, 134 + EntryOffset);
+            stuID.TextChanged += new EventHandler(this.stuCheckID);
+            this.Controls.Add(stuID);
+
+            TextBox stuName = new TextBox();
+            stuName.Name = "stuName" + AthleteCount;
+            stuName.Size = new Size(129, 23);
+            stuName.Location = new Point(118, 134 + EntryOffset);
+            stuName.ReadOnly = true;
+            this.Controls.Add(stuName);
+
+            TextBox stuHouse = new TextBox();
+            stuHouse.Name = "stuHouse" + AthleteCount;
+            stuHouse.Size = new Size(85, 23);
+            stuHouse.Location = new Point(253, 134 + EntryOffset);
+            stuHouse.ReadOnly = true;
+            this.Controls.Add(stuHouse);
+
+            TextBox stuTD = new TextBox();
+            stuTD.Name = "stuTD" + AthleteCount;
+            stuTD.Size = new Size(139, 23);
+            stuTD.Location = new Point(344, 134 + EntryOffset);
+            this.Controls.Add(stuTD);
+
+            TextBox stuPlace = new TextBox();
+            stuPlace.Name = "stuPlace" + AthleteCount;
+            stuPlace.Size = new Size(53, 23);
+            stuPlace.Location = new Point(489, 134 + EntryOffset);
+            this.Controls.Add(stuPlace);
+
+            TextBox stuPoints = new TextBox();
+            stuPoints.Name = "stuPoints" + AthleteCount;
+            stuPoints.Size = new Size(53, 23);
+            stuPoints.Location = new Point(548, 134 + EntryOffset);
+            this.Controls.Add(stuPoints);
+
+            EntryOffset += 25;
+        }
+
+
+        private void btnAddAthlete_Click(object sender, EventArgs e)
+        {
+            // TO AVOID THE ANNOYING BUG
+            this.VerticalScroll.Value = 0;
+
+            AddAthlete();
+        }
+
+        protected void stuCheckID(object sender, EventArgs e)
+        {
+            TextBox tbStuID = sender as TextBox;
+            int AthleteNo = int.Parse(tbStuID.Name.Substring(5));
+
+            TextBox stuNameRef = this.Controls["stuName" + AthleteNo] as TextBox;
+            TextBox stuHouseRef = this.Controls["stuHouse" + AthleteNo] as TextBox;
+
+            if (SportsPlus.studentDictionary.ContainsKey(tbStuID.Text))
+            {
+                stuNameRef.Text = SportsPlus.studentDictionary[tbStuID.Text].Name;
+                stuHouseRef.Text = SportsPlus.studentDictionary[tbStuID.Text].StudentHouse;
+                return;
+            }
+
+            stuNameRef.Text = "";
+            stuHouseRef.Text = "";
+            
         }
     }
 }
